ce0b7fe42eeef6bcea207c0c931cf312a0d5d42b
Saikat048 <mdsaikathosan048@gmail.com>
2022-09-16 10:47:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
+import Navbar from './Pages/Share/Navbar';
+import { Routes, Route } from "react-router-dom";
+import Solution from './Pages/Solution/Solution';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+    
+    <Routes>
+    <Route path="/" element={<Solution></Solution>}></Route>
+    <Route path="/solution" element={<Solution></Solution>}></Route>
+    </Routes>
     </div>
   );
 }
---
+++ src/Pages/Solution/Solution.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Solution = () => {
+    return (
+        <div>
+            <h3>Solution</h3>
+        </div>
+    );
+};
+
+export default Solution;
